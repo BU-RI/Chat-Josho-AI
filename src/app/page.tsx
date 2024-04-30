c265216b8f51ef153ecb269b3803c1ddb1512bab
BU-RI <proaca779@gmail.com>
2024-04-30 14:07:26 +0900

    環境構築完了

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p>Chat JOSHO AI</p>
+      <p>Chat JOSHO</p>
+      <input></input>
       </div>
   );
 }
