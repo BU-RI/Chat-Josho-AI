3ec6a5b10e4507a777fcd7eedaa888559cf71373
BU-RI <proaca779@gmail.com>
2024-06-11 14:58:45 +0900

    scondcommit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,12 +12,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    
+    <html lang="ja">
+      <body>
+        <Header/>
+        {children}
+        </body>
     </html>
   );
 }
